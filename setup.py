e1942fa24c9609d5648ac0e2d3a4bddb5985399a
Ulrich Petri <u.petri@diefirma.de>
2009-11-10 15:13:22 +0100

    Added simple setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name = "django-tiledinline",
+    version = "0.1-f1",
+    packages = find_packages(),
+    author = "TonightsLastSong",
+    author_email = "",
+    description = "",
+    long_description = """
+    """,
+    keywords = "django,admin",
+    url = "",
+)
